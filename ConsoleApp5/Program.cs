a1352be646dc52d87793baa9cbf0c50aec6e707d
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-19 11:27:13 -0300

    exercicios

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor=10;
+            double porcetagem=5;
+            double valorfinal;
+            double.Parse(Console.ReadLine()); 
+
+           
+            valorfinal = (porcetagem * valor / 100);
+
+            valorfinal = valor * (porcetagem / valor );
+                
+           
+                
+
+           Console.WriteLine("O valor final do produto é: " + valorfinal);
+          
+
+          
+
+            
+            
+            
+        }
+
+    }
+    }
+
